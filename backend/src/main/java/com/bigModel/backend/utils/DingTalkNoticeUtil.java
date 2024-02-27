0082585ad9ce9b94dd738153400fec28ab4f36e6
hbhdhd <1072833411@qq.com>
2024-02-27 17:03:32 +0800

    钉钉提醒

--- backend/src/main/java/com/bigModel/backend/utils/DingTalkNoticeUtil.java
+++ backend/src/main/java/com/bigModel/backend/utils/DingTalkNoticeUtil.java
@@ -6,11 +6,15 @@ import com.dingtalk.api.request.OapiMessageCorpconversationAsyncsendV2Request;
 import com.dingtalk.api.response.OapiMessageCorpconversationAsyncsendV2Response;
 import com.taobao.api.ApiException;
 import okhttp3.*;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import twitter4j.JSONObject;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Component
 public class DingTalkNoticeUtil {
@@ -18,17 +22,25 @@ public class DingTalkNoticeUtil {
         String accessToken = getAccessToken();
         DingTalkClient client = new DefaultDingTalkClient("https://oapi.dingtalk.com/topapi/message/corpconversation/asyncsend_v2");
         OapiMessageCorpconversationAsyncsendV2Request request = new OapiMessageCorpconversationAsyncsendV2Request();
-        request.setAgentId(2832368212L);
-        request.setUseridList("260665454821659594, 0233530551481796729358, 284563571837719894");
+        request.setAgentId(2943425497L);
+        // request.setUseridList("260665454821659594, 0233530551481796729358, 284563571837719894");
+        request.setUseridList("01402302401219340");
+        // request.setUseridList("260665454821659594");
         request.setToAllUser(false);
 
         OapiMessageCorpconversationAsyncsendV2Request.Msg msg = new OapiMessageCorpconversationAsyncsendV2Request.Msg();
         msg.setMsgtype("text");
         msg.setText(new OapiMessageCorpconversationAsyncsendV2Request.Text());
-        LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        String date = df.format(now);
-        msg.getText().setContent("发现风险信息" + date);
+        Map<String, String> content = new HashMap<>();
+        content.put("发帖内容", "中华人民共和国");
+        content.put("发帖ID", "112212121212");
+        content.put("发帖时间", "2020-1-1 12:12:12");
+        content.put("发帖链接", "https://baidu.com");
+        content.put("风险提醒原因", "关键词匹配");
+        String result = content.entrySet().stream()
+                        .map(entry -> entry.getKey() + " : " + entry.getValue())
+                                .collect(Collectors.joining(", \n"));
+        msg.getText().setContent(result);
         request.setMsg(msg);
 
 
@@ -46,7 +58,7 @@ public class DingTalkNoticeUtil {
          OkHttpClient client = new OkHttpClient().newBuilder()
                  .build();
          Request request = new Request.Builder()
-                 .url("https://oapi.dingtalk.com/gettoken?appkey=dingksexqekhvsnkpzky&appsecret=QxsgKcezvtCqCcEytHXl4wL6l0sJ9yY12cZ6Ua9rCpDursrg2bXZjgpfJ220c7v1")
+                 .url("https://oapi.dingtalk.com/gettoken?appkey=dingjw2oivs8xtepyvjr&appsecret=3_UZOIFFNKtadcqKJwehmDhUp6iSEOY8vSoj2IIIW17RQf27iSPYwhtPeCRIjTGM")
                  .method("GET", null)
                  .addHeader("User-Agent", "Apifox/1.0.0 (https://apifox.com)")
                  .build();
@@ -55,5 +67,8 @@ public class DingTalkNoticeUtil {
          JSONObject json = new JSONObject(res.string());
          return json.getString("access_token");
      }
-
+    @Scheduled(cron = "0/5 * * * * ?") // 定时 5秒
+    public void xxx() throws Exception {
+        DingTalkNoticeUtil.sendNotice();
+    }
 }
